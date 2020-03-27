fe56dd0d150ffe6102f411a51466c968c473f85e
Utkarsh <utkarshmishra123456789@gmail.com>
2020-03-27 23:32:55 +0530

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 '''
 As you can see, we're now assigning a view called post_list to the root URL.
