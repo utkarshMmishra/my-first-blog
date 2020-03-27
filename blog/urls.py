cf589b17b21387151cab970407cf1184f0c2ded7
Utkarsh <utkarshmishra123456789@gmail.com>
2020-03-27 21:18:29 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 '''
 As you can see, we're now assigning a view called post_list to the root URL.
